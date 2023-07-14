63cbb2a4e5423ff9ea988ddd49df07d2dad81901
Shalaka <shalakav17@gmail.com>
2023-07-14 16:46:41 +0530

    first function in javascript

---
+++ Javascript/firstfunction.js
@@ -0,0 +1,44 @@
+//************function without parameter*********
+// function singSong()
+// {
+//     console.log("Sa");
+//     console.log("Re");
+//     console.log("Ga");
+//     console.log("Ma");
+//     console.log("Pa");
+//     console.log("Dha");
+//     console.log("Ni");
+//     console.log("Sa");
+// }
+// singSong();
+// singSong();
+
+//********function with parameter************
+
+// function greet(fname)
+// {
+//     console.log(`Hii ${fname}`);
+// }
+// greet("Shalaka");
+
+
+// function with more than one parameter
+
+// function repeat(str,num)
+// {
+//     let result='';
+//     for(let i=0;i<num;i++)
+//     {
+//         result += str;
+//     }
+//     console.log(result);
+// }
+// repeat('hii',5);
+
+//return keyword
+
+function addnum(x,y)
+{
+    let sum=x+y;
+    return sum;
+}
