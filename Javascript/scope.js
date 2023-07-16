af7807aa54f0cc13682f6acae9421fbf82fc70fd
Shalaka <shalakav17@gmail.com>
2023-07-16 14:48:56 +0530

    scope of function-block and lexical scope

--- Javascript/scope.js
+++ Javascript/scope.js
@@ -16,11 +16,39 @@
 // days();
 
 
-let day2="Tuesday";
-function days() //scope of variable day2 declare inside function is valid inside function only
+// let day2="Tuesday";
+// function days() //scope of variable day2 declare inside function is valid inside function only
+// {
+//     let day2="Monday";
+//     console.log(day2);
+// }
+// console.log(day2);
+// days();
+
+// Block Scope
+
+// let radius=8;
+// if(radius>0)
+// {
+//     const PI=3.14;
+//     let circ=2*PI*radius;
+// }
+// console.log(radius); // print 8
+// console.log(PI); //  not defined
+// console.log(circ);  // not defined
+
+// Lexical Scope
+
+function game_list()
 {
-    let day2="Monday";
-    console.log(day2);
+    const games=['Cricket','Kho-kho','Football','Basketball'];  // variable defined inside game_list function is accessible inside played_game
+    function played_game()    // variable defined inside played_game is not accessible in game_list function
+    {
+        for(let game of games)
+        {
+            console.log(`Played game is ${game}`);
+        }
+    }
+    played_game();
 }
-console.log(day2);
-days();
+game_list();
