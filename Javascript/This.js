452de5aad2a845b02449421bc1370830129228b7
Shalaka <shalakav17@gmail.com>
2023-07-26 14:54:11 +0530

    This keyword in Javascript

---
+++ Javascript/This.js
@@ -0,0 +1,18 @@
+const language={
+    l1:'Python',
+    l2:'C++',
+    l2:'Java',
+    learn()
+    {
+        console.log(`${this.l2} is learnt by me`);
+    }
+}
+
+// let hen={
+//     name:"Helen",
+//     eggCount:0,
+//     layAnEgg() {
+//         console.log(`${this.eggCount++}`);
+//         return "EGG";
+//     }
+// }
