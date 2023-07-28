6392ed48934e7d2a8f993e3a0d0a96fcfebd9b27
Shalaka <shalakav17@gmail.com>
2023-07-28 14:12:58 +0530

    Array Methods

--- Javascript/Array Methods.js
+++ Javascript/Array Methods.js
@@ -1,4 +1,4 @@
-// For each Method -> calls the function once per element in the array
+// 1)For each Method -> calls the function once per element in the array
 
 const numbers=[1,2,3,4,5,6,7,8,9,10];
 
@@ -24,11 +24,33 @@ const numbers=[1,2,3,4,5,6,7,8,9,10];
 //     }
 // })
 
-// Map Method -> Creates a new array with the results of calling a callback on every element in array  
+// 2) Map Method -> Creates a new array with the results of calling a callback on every element in array  
 
-const doubles=numbers.map(function (num){
-    console.log(num*2);
-})
-4
+// const doubles=numbers.map(function (num){
+//     console.log(num*2);
+// })
 //original array->numbers=[1,2,3,4,5,6,7,8,9,10]
 //output-> doubles=[2,4,6,8,10,12,14,16,18,20]
+
+// 3) Arrow Function
+
+// const Add=(x,y)=>{
+//     return x+y;
+// }
+
+// const RollDie = () =>{
+//     return Math.floor(Math.random()*6)+1
+// }
+
+//Implicit Return Arrow function
+
+const RollDie = () =>(
+    Math.floor(Math.random()*6)+1
+)
+
+const add = (a,b) => a+b   // One liner implicit function and it works when we want to return single line inside a function
+
+console.log("Hello.....")
+setTimeout(()=>{
+    console.log("Are you still there....")
+},3000)
